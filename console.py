e935e12f3fc9a3bf65007ecb5917b14098015a1e
ShroukOuda <s36568961@gmail.com>
2024-05-18 14:41:50 -0400

    commit

--- console.py
+++ console.py
@@ -9,17 +9,17 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         return True
-    
+
     def do_quit(self, line):
         return True
-    
+
     def emptyline(self):
         return super().emptyline()
-    
+
     def help_quit(self):
         print('Quit command to exit the program\n')
 
     def help_EOF(self):
-        print('EOF command to exit the program\n')   
+        print('EOF command to exit the program\n')
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
