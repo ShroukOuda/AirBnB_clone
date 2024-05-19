3f032a19f4676f211efd24c3c65d8473f6da2d05
ShroukOuda <s36568961@gmail.com>
2024-05-19 07:02:37 -0400

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,5 @@
 #!/use/bin/python3
 '''__init__'''
-
 from models.engine.file_storage import FileStorage
-
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 '''base model'''
-
 import uuid
 from datetime import datetime
 from models.__init__ import storage
